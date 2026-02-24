22a19b1f8ed872398d281ceef38ed38d98c098b3
Christian Longos <longoschristiiiaaan@gmail.com>
2026-02-24 11:22:56 +0800

    Add Terraform workflow for production deployment and update S3 region configuration
    This commit introduces a GitHub Actions workflow for deploying Terraform configurations to the production environment. The workflow triggers on pushes to the main branch and includes steps for initialization, planning, and applying Terraform changes. Additionally, the S3 backend region in the versions.tf file has been updated from "sgp1" to "us-east-1" to align with the new deployment strategy.

--- env/prod/versions.tf
+++ env/prod/versions.tf
@@ -4,10 +4,11 @@ terraform {
   backend "s3" {
     bucket                      = "iac-file"
     key                         = "koneksi-prod/terraform.tfstate"
-    region                      = "sgp1"
+    region                      = "us-east-1"
     endpoint                    = "https://sgp1.digitaloceanspaces.com"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
+    skip_region_validation      = true
     force_path_style            = true
   }
 
