519daca3816e1ba3f776f696a6c4fd865c70a1e6
Christian Longos <longoschristiiiaaan@gmail.com>
2026-02-24 11:30:25 +0800

    Update Terraform S3 backend configuration in versions.tf to use new endpoints format and enable account ID skipping

--- env/prod/versions.tf
+++ env/prod/versions.tf
@@ -5,10 +5,11 @@ terraform {
     bucket                      = "iac-file"
     key                         = "koneksi-prod/terraform.tfstate"
     region                      = "us-east-1"
-    endpoint                    = "https://sgp1.digitaloceanspaces.com"
+    endpoints                   = { s3 = "https://sgp1.digitaloceanspaces.com" }
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
+    skip_requesting_account_id  = true
     force_path_style            = true
   }
 
